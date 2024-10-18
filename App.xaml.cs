bb68aa7682fc24154e28a09124b8be3fa09f3641
Salvatore Amaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-10-18 14:21:02 +0100

    update

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,20 +1,23 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 using ZooSimulatorLibrary.Animals;
 using ZooSimulatorLibrary.Zoo;
+using ZooSimulatorLibrary.Zoo.DependencyRegistration;
 
 namespace ZooSimulator
 {
     public partial class App : Application
     {
         public static int NumOfAnimals = 5;
-        public static Zoo Zoo { get; private set; } = new();
+        public static Zoo? Zoo { get; private set; } = ZooServices.Provider.GetService<Zoo>();
 
-        public App() 
+        public App()
         {
         }
 
         protected async override void OnStartup(StartupEventArgs e)
         {
+            if (Zoo == null) throw new NullReferenceException();
             Zoo.Animals = await AnimalGenerator.GenerateZooAsync(NumOfAnimals);
             base.OnStartup(e);
         }
